23a3a9a138697f213029f2a774042eaa3a2b38af
flan <flan@flande.re>
2017-06-21 12:47:23 +0200

    Include a helper program for listing guilds and channels

---
+++ listcord/main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+var (
+	username = flag.String("user", "", "email address")
+	password = flag.String("pass", "", "password")
+	token    = flag.String("t", "", "access token")
+	summary  = flag.Bool("s", false, "don't list channels")
+)
+
+func list(d *discordgo.Session, event *discordgo.Ready) {
+	gid := ""
+	for {
+		guilds, err := d.UserGuilds(100, "", gid)
+		if err != nil {
+			log.Fatal("error getting guilds:", err)
+		}
+
+		if len(guilds) == 0 {
+			break
+		}
+
+		for _, i := range guilds {
+			gid = i.ID
+
+			if flag.Arg(0) != "" && gid != flag.Arg(0) {
+				continue
+			}
+
+			fmt.Println(gid, i.Name)
+
+			if *summary {
+				continue
+			}
+
+			g, err := d.Guild(gid)
+			if err != nil {
+				log.Printf("error getting guild %s: %s", gid, err)
+				continue
+			}
+
+			for _, i := range g.Channels {
+				var symbol string
+
+				switch i.Type {
+				case "text":
+					symbol = "#"
+				case "voice":
+					symbol = "🔊 "
+				default:
+					symbol = "(?) "
+				}
+
+				fmt.Println("\t", i.ID, symbol+i.Name)
+			}
+		}
+	}
+	os.Exit(0)
+}
+
+func main() {
+	flag.Parse()
+
+	d, err := discordgo.New(*username, *password, *token)
+	if err != nil {
+		log.Fatal("login failed:", err)
+	}
+
+	err = d.Open()
+	defer d.Close()
+	if err != nil {
+		log.Fatal("opening the websocket connection failed:", err)
+	}
+
+	d.AddHandler(list)
+
+	if *token == "" {
+		log.Println("token:", d.Token)
+	}
+
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+}
--- main.go
+++ main.go
@@ -2,43 +2,53 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/bwmarrin/discordgo"
 )
 
 var (
-	username = flag.String("u", "", "username")
-	password = flag.String("p", "", "password")
+	username = flag.String("user", "", "email address")
+	password = flag.String("pass", "", "password")
 	token    = flag.String("t", "", "access token")
+
+	cid  = flag.String("c", "", "comma-separated channel IDs to include")
+	gid  = flag.String("s", "", "comma-separated server IDs to include")
+	xcid = flag.String("C", "", "comma-separated channel IDs to exclude")
+	xgid = flag.String("S", "", "comma-separated server IDs to exclude")
+
+	continuous = flag.Bool("continuous", false, "keep archiving in background after fetching the whole history")
 )
+
+func do(d *discordgo.Session, event *discordgo.Ready) {
+	log.Println("stub")
+	os.Exit(0)
+}
 
 func main() {
 	flag.Parse()
+
 	d, err := discordgo.New(*username, *password, *token)
 	if err != nil {
 		log.Fatal("login failed:", err)
 	}
 
+	err = d.Open()
+	defer d.Close()
+	if err != nil {
+		log.Fatal("opening the websocket connection failed:", err)
+	}
+
+	d.AddHandler(do)
+
 	if *token == "" {
 		log.Println("token:", d.Token)
 	}
 
-	gid := ""
-	for {
-		guilds, err := d.UserGuilds(100, "", gid)
-		if err != nil {
-			log.Fatal("error getting guilds:", err)
-		}
-
-		if len(guilds) == 0 {
-			break
-		}
-
-		for _, i := range guilds {
-			fmt.Println(i.ID, i.Name)
-			gid = i.ID
-		}
-	}
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
 }
