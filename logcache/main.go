tsudoko/pullcord

commit 96d3a95c3c88f677d1c0cbc3d33b85dfbbff91ee

Update entry cache after writing to log

logcache/main.go
@@ -55,6 +55,7 @@ func (cache *Entries) WriteNew(w io.Writer, e []string) {
 	if len(cacheEntry) < logentry.HTime+1 || len(e) < logentry.HTime+1 ||
 		!entryEquals(cacheEntry[logentry.HTime+1:], e[logentry.HTime+1:]) {
 		tsv.Write(w, e)
+		(*cache)[e[logentry.HType]][e[logentry.HID]] = e
 	}
 }
 

commit 2d21a1c8e8fd3b2d69cd54900db501352b930053

Create cache if it doesn't exist
Fixes a regression introduced in 96d3a95 (#20).

logcache/main.go
@@ -55,6 +55,9 @@ func (cache *Entries) WriteNew(w io.Writer, e []string) {
 	if len(cacheEntry) < logentry.HTime+1 || len(e) < logentry.HTime+1 ||
 		!entryEquals(cacheEntry[logentry.HTime+1:], e[logentry.HTime+1:]) {
 		tsv.Write(w, e)
+		if (*cache)[e[logentry.HType]] == nil {
+			(*cache)[e[logentry.HType]] = make(map[string][]string)
+		}
 		(*cache)[e[logentry.HType]][e[logentry.HID]] = e
 	}
 }